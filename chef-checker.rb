5ebb0e6aaaee5b1ce613b4132305554c0d0fcc6a
David Jacovkis <david@freeknowledge.eu>
2013-02-12 13:35:55 +0100

    Cleanup and comply with Nagios plugin guidelines http://nagiosplug.sourceforge.net/developer-guidelines.html#AEN76

--- chef-checker.rb
+++ chef-checker.rb
@@ -2,39 +2,75 @@
 
 require "time"
 require 'optparse'
+require 'debugger'
 
 class ChefClientStatus
 
   def initialize()
     # Parse command line options and set default values
     opts = Hash.new
-    opts[:debug] = false
-    opts[:max_time] = 1800
-    opts[:status_file] = "chef-status"
     opts[:log_file] = "/var/log/chef/client.log"
+    opts[:debug] = false
   
-    OptionParser.new do |o|
-      o.on('-d') { |b| opts[:debug] = b }
-      o.on('-m MAXTIME') { |secs| opts[:max_time] = secs }
-      o.on('-s STATUS_FILE') { |filename| opts[:status_file] = filename }
+    oparse = OptionParser.new do |o|
+      o.banner = "Usage: chef-checker.rb [options]"
+      o.separator ""
+      o.separator "Specific options:"
+      
+      o.on("-vv", "Turn debug mode on") do |b|
+         opts[:debug] = b
+      end
+      o.on("-w WARNTIME",
+      "Exit with Warning status if elapsed time since last",
+      " chef-client run exceeds WARNTIME") do |secs|
+        opts[:warn_time] = secs.to_i
+      end
+      o.on('-c CRITTIME',
+      "Exit with Critical status if elapsed time since last",
+      " chef-client run exceeds CRITTIME") do |secs|
+        opts[:crit_time] = secs.to_i
+      end
       o.on('-l LOG_FILE') { |filename| opts[:log_file] = filename }
-      o.on('-h') { puts o; exit }
-      o.parse!
+      
+      o.separator ""
+      o.separator "Common options:"
+      
+      o.on('-h', '--help', "This message") { puts o; exit }
     end
-
+    
+    begin
+      oparse.parse!
+      mandatory = [:warn_time, :crit_time]
+      missing = mandatory.select{ |par| opts[par].nil?}
+      if not missing.empty?
+        puts "Missing options: #{missing.join(", ")}"
+        puts oparse
+        exit
+      end
+    end
+      
+    
+    @service_status = {
+      0 => "OK",
+      1 => "Warning",
+      2 => "Critical",
+      3 => "Unknown"
+    }
+    
+    @logfile = opts[:log_file]
     @debug = opts[:debug]
-    @max_elapsed_time = opts[:max_time]
+    @warn_time = opts[:warn_time]
+    @crit_time = opts[:crit_time]
     @status = {
-      :state_code => 0,
+      :state_code => 3,  # initial status is "Unknown"
       :state_info => ""      
     }
    	logfile = opts[:log_file]
-    @status_file = opts[:status_file]
-    @hostname = `hostname`.chomp
+    @hostname = `/bin/hostname`.chomp
     
     # typical chef-client runs in www servers are ~350 lines
     tail_lines = 1000    
-    cmd = "tail -n" + " " + tail_lines.to_s +  " " + logfile
+    cmd = "/usr/bin/tail -n" + " " + tail_lines.to_s +  " " + logfile
     log_tail = `#{cmd}`
     log_tail_a = []
     @last_run = []
@@ -46,12 +82,11 @@ class ChefClientStatus
     
   def run
     process()
-    submit()
+    output()
   end
   
   def process()
     # extract the date from the last line of the log
-    # last_finished = @last_run[-1]
     date_s = @last_run[-1].split(']')[0][1..-1]
     t_lastrun = Time.parse(date_s)
     t_now = Time.new
@@ -59,37 +94,40 @@ class ChefClientStatus
     # search for ERROR or FATAL messages in the log
     @last_run.reverse_each do |line|
       if ((line["ERROR"]) or (line["FATAL"]))
-        @status[:state_code] = 2
-        @status[:state_info] = "The last run has errors. Last error message: \"#{line}\""
+        @status[:state_code] = 2  # set the state to Critical
+        @status[:state_info] = "The last run failed"
+        @status[:state_extinfo] = "Last error message: \"#{line}\""
         return
       end
     end
     
     # check the time elapsed since last run ended
     elapsed_time = t_now - t_lastrun
-    if (elapsed_time > @max_elapsed_time)
+    if (elapsed_time > @warn_time)
       h = (elapsed_time/3600).to_i
       m = (elapsed_time/60 - h*60).to_i
       s = (elapsed_time - m*60 - h*3600).to_i
-      @status[:state_code] = 2
-      @status[:state_info] = "Last run finished #{h}:#{m}:#{s} ago."
-      return
+      @status[:state_code] = 1  # set state to Warning
+      @status[:state_info] = "Last run finished #{h}h #{m}m ago"
+      @status[:state_extinfo] = elapsed_time.to_i.to_s + "s"
+      if (elapsed_time > @crit_time)
+          @status[:state_code] = 2  # set state to Critical
+      end
+      return      
     end
     
-    @status[:state_code] = 0    
+    @status[:state_code] = 0 # Status is OK    
   end
   
-  def submit()
-    data = "#{@hostname}\tchef-client\t#{@status[:state_code]}\t#{@status[:state_info]}\n"
-    File.open(@status_file, 'w') {|f| f.write(data) }
+  def output()
+    service_status = @service_status[@status[:state_code]] 
+    data = "chef-client #{service_status} - #{@status[:state_info]}|#{@status[:state_extinfo]}\n"
     if(@debug)
         puts "The status of the service is #{@status[:state_code].to_s}."
         puts "The state info is: #{@status[:state_info]}"
-        puts "Submission data written to #{@status_file}"
+        puts "Log file analysed: #{@logfile}"
     else
-        output = `/usr/sbin/send_nsca icinga1a.zinio.com -c /etc/send_nsca.cfg < #{@status_file}`
-        output = `/usr/sbin/send_nsca icinga1b.zinio.com -c /etc/send_nsca.cfg < #{@status_file}`
-        output = `/usr/sbin/send_nsca icingaweb1.zinio.com -c /etc/send_nsca.cfg < #{@status_file}` 
+        puts data
     end
   end
 end
